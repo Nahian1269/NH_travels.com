e8f5a2b2c6f5bb525893e8f77743b1be5644eab5
Kazi Aynun Nahin Dip <103853468+Nahian1269@users.noreply.github.com>
2024-02-20 03:11:11 +0600

    new

--- styles/style.js
+++ styles/style.js
@@ -17,13 +17,20 @@ function reload() {
   cartelement.innerHTML = "";
   add.forEach((item) => {
     const li = document.createElement("li");
-    li.textContent = `${item.name}---------------------${item.name2}--------------$${item.price}`;
+    li.style.listStyle="none";
+    li.textContent = `${item.name}---------------------${item.name2}-----------------$${item.price}`;
     cartelement.appendChild(li);
   });
 
   let counter = num++;
   let caller = document.getElementById("cc");
   caller.textContent = num;
+
+  if(num>4){
+    alert("can't Purches more than 4");
+    const took = document.getElementById("btn-2"); 
+    took.style.display="none" ;
+  }
 
   let calc = --t_seat;
   let must = document.getElementById("XL");
@@ -34,6 +41,13 @@ function reload() {
     let dis = document.getElementById("count");
   }
 
+  // let cat = document.getElementById("del"); 
+  // cat.addEventListener('click', () => {
+  //   const listIt = document.getElementById("li") ; 
+  //   const lastitem = listIt.lastElementChild ; 
+  //   listIt.removeChild(lastitem);
+  // });
+  
   pricer();
 }
 
@@ -42,6 +56,8 @@ function pricer() {
   let call = document.getElementById("Gp");
   call.textContent = "BDT" + price1;
   Cuppon();
+  ripper();
+  couple();
 }
 
 function Cuppon() {
@@ -53,8 +69,70 @@ function Cuppon() {
   }
 }
 
-function  clickme() {
+document.getElementById('btn-2').addEventListener('click', function() {
+  document.getElementById('success').style.display = 'block';
+});
 
-    document.getElementById("success").hidden= false ;
+document.getElementById('close').addEventListener('click', function() {
+  document.getElementById('success').style.display = 'none';
+});
+
+function clickme(){
+  document.getElementById("press").style.display="none"
+  document.getElementById("cns").style.display="none"
+  document.getElementById("lol").style.display="none"
+  document.getElementById("tot").style.display="none"
+}
+
+function pres(){
+  document.getElementById("press").style.display="block"
+  document.getElementById("cns").style.display="block"
+  document.getElementById("lol").style.display="block"
+  document.getElementById("tot").style.display="block"
+}
+
+function ripper(){
+
+  document.getElementById('roundup').addEventListener('click', function() {
+    var couponInput = document.getElementById('box').value;
+    var validCoupon ="NEW15";
+    var validCoupon2="Couple20" ;
+    
+    if (couponInput === validCoupon) {
+      let take = total_price;
+      let cut = parseInt(take * 0.15);
+      let final = take - cut;
+      document.getElementById("Gp").textContent = final;
+    } 
+ 
+
+  });  
+
+
+  
 
 }
+
+
+function couple(){
+
+  document.getElementById('roundup').addEventListener('click', function() {
+    var couponInput = document.getElementById('box').value;
+    var validCoupon2="Couple20" ;
+    
+    if (couponInput === validCoupon2) {
+      let take = total_price;
+      let cut = parseInt(take * 0.2);
+      let final = take - cut;
+      document.getElementById("Gp").textContent = final;
+    } 
+
+
+  });  
+
+}
+
+
+
+ 
+
